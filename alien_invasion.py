1e38bd95b85f0993558f2ea17edd979a9e3d6e62
sjenterrement <sjenterrement@gmail.com>
2019-04-24 13:30:32 +0800

    Update alien_invasion.py
    添加怪兽

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -13,20 +14,23 @@ def run_game():
         (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
-    ship=Ship(ai_settings,screen)
-    # 创建一个用于存储子弹的编组
-    bullets=Group()
-
-    #设置背景色
+    # 设置背景色
     bg_color=(230,230,230)
 
+    #创建一艘飞船,一个子弹编组,一个外星人编组
+    ship=Ship(ai_settings,screen)
+    bullets=Group()
+    aliens=Group()
+
+    # 创建外星人
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+    
     #开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
         
