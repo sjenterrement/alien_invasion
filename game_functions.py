dbec5e1ffe44e01e49c5409a6b4a0cd43e767f43
sjenterrement <sjenterrement@gmail.com>
2019-04-29 18:05:38 +0800

    Update game_functions.py
    射杀外星人

--- game_functions.py
+++ game_functions.py
@@ -53,15 +53,25 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(ai_settings,screen,ship,aliens,bullets):
     '''更新子弹位置，并删除已消失的子弹'''
     # 更新子弹位置
     bullets.update()
-
     # 删除已消失的子弹
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets)
+
+def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
+    '''相应子弹和外星人的碰撞'''
+    # 删除发生碰撞的子弹和外星人
+    collisions=pygame.sprite.groupcollide(bullets,aliens,True,True)
+    if len(aliens)==0:
+        # 删除现有的子弹并创建一群外星人
+        bullets.empty()
+        create_fleet(ai_settings,screen,ship,aliens)
+
 
 def get_number_aliens_x(ai_settings,alien_width):
     '''计算每行可容纳多少个外星人'''
