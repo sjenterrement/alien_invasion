4131661749fa1fc7c8ff575a6ef519596ab00a56
sjenterrement <sjenterrement@gmail.com>
2019-05-01 14:36:03 +0800

    Update alien_invasion.py
    13end

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 from alien import Alien
 import game_functions as gf
+from game_stats import GameStats
 
 def run_game():
     #初始化gygame、设置和屏幕对象
@@ -13,6 +14,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一个用于存储游戏统计信息的实例
+    stats=GameStats(ai_settings)
 
     # 设置背景色
     bg_color=(230,230,230)
@@ -28,9 +32,12 @@ def run_game():
     #开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-        gf.update_aliens(ai_settings,aliens)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+            
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
