b8d76ce59e4c5b3f6e474eceff8e4f9e9b11e7e8
sjenterrement <sjenterrement@gmail.com>
2019-05-05 22:23:09 +0800

    Update game_functions.py
    5.5

--- game_functions.py
+++ game_functions.py
@@ -28,7 +28,7 @@ def check_keyup_events(event,ship):
     elif event.key==pygame.K_LEFT:
         ship.moving_left=False
 
-def check_events(ai_settings,screen,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,bullets):
     '''响应按键和鼠标时间'''
     for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -37,18 +37,28 @@ def check_events(ai_settings,screen,ship,bullets):
                 check_keydown_events(event,ai_settings,screen,ship,bullets)
             elif event.type==pygame.KEYUP:
                 check_keyup_events(event,ship)
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                mouse_x,mouse_y=pygame.mouse.get_pos()
+                check_play_button(stats,play_button,mouse_x,mouse_y)
+def check_play_button(stats,play_button,mouse_x,mouse_y):
+    '''在玩家单击Play按钮时开始游戏'''
+    if play_button.rect.collidepoint(mouse_x,mouse_y):
+        stats.game_active=True
 
-def update_screen(ai_settings,screen,ship,aliens,bullets):
+def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
     '''更新屏幕上的图像，并且换到新屏幕'''
     #每次循环都重绘屏幕
     screen.fill(ai_settings.bg_color)
 
     # 在飞船和外星人后面重绘所有子弹
     for bullet in bullets.sprites():
-        bullet.draw_bullet()
-        
+        bullet.draw_bullet()   
     ship.blitme()
     aliens.draw(screen)
+
+    # 如果游戏处于非活动状态，就绘制Play按钮
+    if not stats.game_active:
+        play_button.draw_button()
     
                 
     #让最近绘制的屏幕可见
