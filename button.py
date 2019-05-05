b11d47ba35b1c08835c34696ab43df89a8aaa555
sjenterrement <sjenterrement@gmail.com>
2019-05-05 22:23:03 +0800

    Update button.py
    5.5

--- button.py
+++ button.py
@@ -10,7 +10,7 @@ class Button():
         # 设置按钮的尺寸和其他属性
         self.width,self.height=200,50
         self.button_color=(0,255,0)
-        self.text_color(255,255,255)
+        self.text_color=(255,255,255)
         self.font=pygame.font.SysFont(None,48)
 
         # 创建按钮的rect对象，并使其居中
