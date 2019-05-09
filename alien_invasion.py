bd8a3c795d2befb45ee4cf25905dd6367f40ec61
sjenterrement <sjenterrement@gmail.com>
2019-05-09 22:27:51 +0800

    Update alien_invasion.py
    5.9

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,14 +35,14 @@ def run_game():
     
     #开始游戏的主循环
     while True:
-        gf.check_events(ai_settings,screen,stats,play_button,ship,bullets)
+        gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
             gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
             
-        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
+        gf.update_screen(ai_settings,screen,stats,play_button,ship,aliens,bullets)
 
 run_game()
         
