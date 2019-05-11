bae657010e5f1a1028cca7f5faa2ddcfd30d5da5
sjenterrement <sjenterrement@gmail.com>
2019-05-11 22:20:43 +0800

    Update alien_invasion.py
    5.11

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ from alien import Alien
 import game_functions as gf
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
+
 
 def run_game():
     #初始化gygame、设置和屏幕对象
@@ -19,8 +21,9 @@ def run_game():
     # 创建Play按钮
     play_button=Button(ai_settings,screen,"Play")
 
-    # 创建一个用于存储游戏统计信息的实例
+    # 创建一个用于存储游戏统计信息的实例,并创建计分牌
     stats=GameStats(ai_settings)
+    sb=Scoreboard(ai_settings,screen,stats)
 
     # 设置背景色
     bg_color=(230,230,230)
@@ -35,14 +38,14 @@ def run_game():
     
     #开始游戏的主循环
     while True:
-        gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
+        gf.check_events(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
             gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
             
-        gf.update_screen(ai_settings,screen,stats,play_button,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 
 run_game()
         
