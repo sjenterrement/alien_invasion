f06602621ccfea92c436291f36d42e754d0ee24c
sjenterrement <sjenterrement@gmail.com>
2019-05-14 17:27:51 +0800

    Update game_functions.py
    14.3.7

--- game_functions.py
+++ game_functions.py
@@ -84,7 +84,7 @@ def update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button):
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(ai_settings,screen,ship,aliens,bullets):
+def update_bullets(ai_settings,screen,stats,sb,ship,aliens,bullets):
     '''更新子弹位置，并删除已消失的子弹'''
     # 更新子弹位置
     bullets.update()
@@ -92,12 +92,19 @@ def update_bullets(ai_settings,screen,ship,aliens,bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets)
+    check_bullet_alien_collisions(ai_settings,screen,stats,sb,ship,aliens,bullets)
 
-def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
+def check_bullet_alien_collisions(ai_settings,screen,stats,sb,ship,aliens,bullets):
     '''相应子弹和外星人的碰撞'''
     # 删除发生碰撞的子弹和外星人
     collisions=pygame.sprite.groupcollide(bullets,aliens,True,True)
+
+    if collisions:
+        for aliens in collisions.values():
+            stats.score+=ai_settings.alien_points*len(aliens)
+            sb.prep_score()
+        check_high_score(stats,sb)
+        
     if len(aliens)==0:
         # 删除现有的子弹,加快游戏节奏，并创建一群外星人
         bullets.empty()
@@ -192,3 +199,9 @@ def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
 
     # 检查是否有外星人到达屏幕低端
     check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
+
+def check_high_score(stats,sb):
+    '''检查是否诞生了新的最高得分'''
+    if stats.score>stats.high_score:
+        stats.high_score=stats.score
+        sb.prep_high_score()
