eddc15169ef338da8e8c82e14b8505e61588505d
sjenterrement <sjenterrement@gmail.com>
2019-05-15 22:16:33 +0800

    Update scoreboard.py
    14.3.8

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,7 @@ class Scoreboard():
         # 准备当前最高得分和当前得分的图像
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         '''将得分转换为一幅渲染的图像'''
@@ -35,6 +36,7 @@ class Scoreboard():
         '''在屏幕上显示得分'''
         self.screen.blit(self.score_image,self.score_rect)
         self.screen.blit(self.high_score_image,self.high_score_rect)
+        self.screen.blit(self.level_image,self.level_rect)
 
     def prep_high_score(self):
         '''将最高得分转换为渲染的图像'''
@@ -47,5 +49,15 @@ class Scoreboard():
         self.high_score_rect=self.high_score_image.get_rect()
         self.high_score_rect.centerx=self.screen_rect.centerx
         self.high_score_rect.top=self.score_rect.top
+
+    def prep_level(self):
+        '''将等级转换为渲染的图像'''
+        self.level_image=self.font.render(str(self.stats.level),True,
+                self.text_color,self.ai_settings.bg_color)
+
+        # 将等级放在得分下方
+        self.level_rect=self.level_image.get_rect()
+        self.level_rect.right=self.score_rect.right
+        self.level_rect.top=self.score_rect.bottom+10
         
                                         
